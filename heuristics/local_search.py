e99ce85a1872aa591e0cf8c1a314e277b1d84bf5
Alicja Mazur <mazur.alicja4@gmail.com>
2023-12-09 19:42:53 +0100

    fix base file

--- heuristics/local_search.py
+++ heuristics/local_search.py
@@ -1,55 +1,15 @@
-from dataclasses import dataclass
-from typing import List
 import sys
 import logging
+from utils import Solution
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(sys.stdout)
 logger.addHandler(handler)
 
-OrderSchedule = List[bool]
-
-OrderSchedule = List[bool]
-
-@dataclass
-class Input:
-    n: int
-    t: int
-    profit: List[int]
-    length: List[int]
-    max_delivery: List[int]
-    min_delivery: List[int]
-    surface: List[int]
-    surface_capacity: int
-
-
-@dataclass
-class Order:
-    id: str
-    profit: int
-    length: int
-    min_delivery: int
-    max_delivery: int
-    surface: int
-
-
-@dataclass
-class TimeSlotCapacity:
-    id: str
-    free_surface: int
-
-
-@dataclass
-class Solution:
-    taken_orders: List[bool]
-    profit: int
-    schedule: List[OrderSchedule]
-    occupation: List[TimeSlotCapacity]
-
 
 class LocalSearch:
 
     def solve(self, initial_solution: Solution) -> Solution:
-        return 12
+        pass
 
